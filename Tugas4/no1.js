06a5cf7440383dcae4ed6aed95e071ea12d4f9a1
aisubea <rmmakmal@gmail.com>
2025-10-18 20:46:41 +0700

    Add files via upload

---
+++ Tugas4/no1.js
@@ -0,0 +1,15 @@
+let studentsScore = [
+    { name: 'Andi', score: 90 },
+    { name: 'Rudi', score: 80 },
+    { name: 'Dira', score: 100 }
+];
+
+let highest = studentsScore[0];
+
+for (let i = 1; i < studentsScore.length; i++) {
+    if (studentsScore[i].score > highest.score) {
+        highest = studentsScore[i];
+    }
+}
+
+console.log(`Siswa dengan nilai tertinggi adalah ${highest.name} dengan nilai ${highest.score}`);
